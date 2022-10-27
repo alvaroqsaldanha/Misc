85f349d0febd288603d793e1bf4f0d9d27131883
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2022-10-27 11:46:15 +0100

    Added exs

--- CTCIexercises/Java/ex.java
+++ CTCIexercises/Java/ex.java
@@ -41,4 +41,46 @@ class ex {
         return true;
     }
 
+    // One Away 
+    static boolean oneditaway(String str1, String str2) {
+        if (Math.abs(str1.length() - str2.length()) > 1) {
+            return false;
+        }
+
+        String longer_str = str1.length() < str2.length() ? str2 : str1;
+        String shorter_str = str1.length() < str2.length() ? str1 : str2;
+
+        int index_long = 0;
+        int index_short = 0;
+        boolean found_diff = false;
+        while (index_long < longer_str.length() && index_short < shorter_str.length()) {
+            if (longer_str.charAt(index_long) != shorter_str.charAt(index_short)) {
+                if (found_diff) return false;
+                found_diff = true;
+                if (longer_str.length() == shorter_str.length()) {
+                    index_short++;
+                }
+            }
+            else {
+                index_short++;
+            }
+            index_long++;
+        } 
+        return true;
+    }
+
+    // String Compression
+    static String stringcompression(String str) {
+        String compressedString = "";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+            if (i + 1 == str.length() || str.charAt(i) !=  str.charAt(i+1)) {
+                compressedString += str.charAt(i) + count;
+                count = 0;
+            }
+        }
+        return compressedString.length() > str.length() ? str : compressedString;
+    }
+
 }  
