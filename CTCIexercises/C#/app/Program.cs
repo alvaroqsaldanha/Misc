60595558433d79640ada6b18a97ed785ff2782eb
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2022-12-01 14:58:31 +0000

    Adding files

---
+++ CTCIexercises/C#/app/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI
+{
+    class CTCI {         
+        static void Main(string[] args)
+        {
+            LinkedListNode head = new LinkedListNode(1);
+            head.insert(2);
+            head.insert(3);
+            head.insert(4);
+            head.insert(2);
+            head.insert(3);
+            head.insert(3);
+            deleteDups(head);
+            head.printList();
+        }
+
+    static void deleteDups(LinkedListNode head) {
+        HashSet<int> buffer = new HashSet<int>();
+        LinkedListNode prev = null;
+        LinkedListNode n = head;
+        while (n != null) {
+            if (buffer.Contains(n.data)) {
+                prev.next = n.next;
+            }
+            else {
+                buffer.Add(n.data);
+                prev = n;
+            }
+            n =  n.next;
+        }
+    }
+    }
+
+    class LinkedListNode {
+
+        public int data;
+        public LinkedListNode next;
+
+        public LinkedListNode(int d) {
+            data = d;
+            next = null;
+        }
+
+        public void insert(int d) {
+            LinkedListNode n = this;
+            LinkedListNode add = new LinkedListNode(d);
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = add;
+        }
+
+        public void printList() {
+            LinkedListNode n = this;
+            while (n != null) {
+                Console.WriteLine(n.data);
+                n = n.next;
+            }
+        }
+    }
+
+
+}
--- CTCIexercises/Java/ex.java
+++ CTCIexercises/Java/ex.java
@@ -83,4 +83,20 @@ class ex {
         return compressedString.length() > str.length() ? str : compressedString;
     }
 
+    // Rotate Matrix
+    static void rotatematrix(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = i; j < len - i - 1;j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len-1-j][i];
+                matrix[len-1-j][i] = matrix[len-1-i][len-1-j];
+                matrix[len-1-i][len-1-j] = matrix[j][len-1-i];
+                matrix[j][len-1-i] = temp;
+            }
+        }
+    }
+
+    // String Rotation
+    
 }  
