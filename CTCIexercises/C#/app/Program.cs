03176bcc5fe407f8f32a822303438eb87132a89f
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2023-01-21 15:41:02 +0000

    Starting C# exs

--- CTCIexercises/C#/app/Program.cs
+++ CTCIexercises/C#/app/Program.cs
@@ -1,246 +1,65 @@
 ﻿using System.Collections;
+using System.Text;
 
 namespace CTCI
 {
     class CTCI {         
         static void Main(string[] args)
         {
-            int[] arr = {1,2,3,4,5,6,7,8};
-            TreeNode node = Trees.minimalTree(arr,0,arr.Length - 1);
-            List<TreeLinkedListNode> lists = Trees.listOfDepths(node);
-            foreach (TreeLinkedListNode list in lists)
-                Console.WriteLine(list);
+            Console.WriteLine(urlify("Mr John Smith    ",13));
         }
 
-        static void deleteDups(LinkedListNode head) {
-            HashSet<int> buffer = new HashSet<int>();
-            LinkedListNode prev = null;
-            LinkedListNode n = head;
-            while (n != null) {
-                if (buffer.Contains(n.data)) {
-                    prev.next = n.next;
-                }
-                else {
-                    buffer.Add(n.data);
-                    prev = n;
-                }
-                n =  n.next;
-            }
-        }
-
-        // Intersection
-        static LinkedListNode intersection(LinkedListNode head1, LinkedListNode head2) {
-            LinkedListNode temp1 = head1;
-            LinkedListNode temp2 = head2;
-            int length1 = 0;
-            int length2 = 0;
-            while (temp1 != null) {
-                length1++;
-                temp1 = temp1.next;
-            }
-            while (temp2 != null) {
-                length2++;
-                temp2 = temp2.next;
-            }
-            int diff = Math.Abs(length1 - length2);
-            temp1 = length1 > length2 ? head1 : head2;
-            temp2 = length1 > length2 ? head2 : head1;
-            for (int i = 0; i < diff; i++) {
-                temp1 = temp1.next;
-            }
-            while (temp1 != temp2) {
-                temp1 = temp1.next;
-                temp2 = temp2.next;
-            }
-            return temp1;
-        }
-
-        // Loop Detection
-        static LinkedListNode loopDetection(LinkedListNode head) {
-            if (head == null)
-                return null;
-            LinkedListNode slow = head;
-            LinkedListNode runner = head.next;
-            while (runner != null && runner.next != null) {
-                if (slow == runner) {
-                    break;
-                }
-                slow = slow.next;
-                runner = runner.next.next;
-            }
-            if (runner == null || runner.next == null) {
-                return null;
-            }
-            slow = head;
-            while (slow != runner) {
-                slow = slow.next;
-                runner = runner.next;
-            }
-            return slow;
-
-        }
-
-        // Delete Middle Node
-        static void deleteMiddleNode(LinkedListNode del) {
-            LinkedListNode n = del;
-            n.data = n.next.data;
-            n.next = n.next.next;
-        }
-
-        //Sum Lists
-        static LinkedListNode sumLists(LinkedListNode head1, LinkedListNode head2) {
-            LinkedListNode temp1 = head1;
-            LinkedListNode temp2 = head2;
-            LinkedListNode sum = null;
-            LinkedListNode head = null;
-            int carry = 0;
-            while (temp1 != null && temp2 != null) {
-                if (sum == null) {
-                    sum = new LinkedListNode(0);
-                    head = sum;
-                }
-                else {
-                    sum.next = new LinkedListNode(0);
-                    sum = sum.next;
-                }
-                int val = (temp1.data + temp2.data + carry) % 10;
-                carry = (temp1.data + temp2.data + carry) / 10;
-                sum.data = val;
-                temp1 = temp1.next;
-                temp2 = temp2.next;
-            } 
-            while (temp1 != null) {
-                sum.next = new LinkedListNode(0);
-                sum = sum.next;
-                int val = (temp1.data + carry) % 10;
-                carry = (temp1.data + carry) / 10;
-                sum.data = val;
-                temp1 = temp1.next;            
-            }  
-            while (temp2 != null) {
-                sum.next = new LinkedListNode(0);
-                sum = sum.next;
-                int val = (temp2.data + carry) % 10;
-                carry = (temp2.data + carry) / 10;
-                sum.data = val;
-                temp1 = temp2.next;            
-            }
-            if (carry == 1) {
-                sum.next = new LinkedListNode(1);
-            }
-            return head;
-        }
-
-        static LinkedListNode sumListsRecursive(LinkedListNode head1, LinkedListNode head2, int carry) {
-            int value = 0;
-            if (head1 != null) {
-                value += head1.data;
-            }
-            if (head2 != null) {
-                value += head2.data;
-            }
-            value += carry;
-            carry = value / 10;
-            LinkedListNode result = null;
-            if (value == 0 && head1 == null && head2 == null) {
-                return null;
-            }
-            else {
-                result = new LinkedListNode(value % 10);
-            }
-            result.next = sumListsRecursive(head1 == null ? null : head1.next,head2 == null ? null : head2.next,carry);
-            return result; 
-        }
-
-        // Is Palindrome
-        static bool isPalindrome(LinkedListNode head) {
-            LinkedListNode slow = head;
-            LinkedListNode runner = head;
-            Stack<int> st = new Stack<int>();
-            while (runner != null && runner.next != null) {
-                st.Push(slow.data);
-                slow = slow.next;
-                runner = runner.next.next;
-            }
-            if (runner != null) {
-                slow = slow.next;
-            }
-            while (slow != null) {
-                if (st.Pop() != slow.data) {
+        // Is Unique: Implement an algorithm to determine if a string has all unique characters.
+        static bool isUnique(string str) {
+            HashSet<int> check = new HashSet<int>();
+            for (int i = 0; i < str.Length; i++) {
+                if (check.Contains(str[i])) {
                     return false;
                 }
-                slow = slow.next;
-            }
-            return true;
-        }
-
-        // Is Palindrome Recursive Auxiliar Function
-        static LinkedListNode isPalindromeRecursiveAux(LinkedListNode node,int length) {
-            if (length == 0) {
-                return node;
-            }
-            else if(length == 1) {
-                return node.next;
-            }
-            LinkedListNode tocompare = isPalindromeRecursiveAux(node.next,length - 2);
-            if (tocompare == null) {
-                return null;
-            }
-            if (tocompare.data == node.data) {
-                if (tocompare.next == null) {
-                    return tocompare;
+                else {
+                    check.Add(str[i]);
                 }
-                return tocompare.next;
-            }
-            else {
-                return null;
-            }
-
-        }
-
-        // Is Palindrome Recursive
-        static bool isPalindromeRecursive(LinkedListNode head) {
-            int length = 0;
-            LinkedListNode temp = head;
-            while (temp != null) {
-                length++;
-                temp = temp.next;
-            }
-            LinkedListNode result = isPalindromeRecursiveAux(head,length);
-            if (result == null) {
-                return false;
             }
             return true;
         }
 
-    }
-
-    class LinkedListNode {
-
-        public int data;
-        public LinkedListNode next;
-
-        public LinkedListNode(int d) {
-            data = d;
-            next = null;
-        }
-
-        public void insert(int d) {
-            LinkedListNode n = this;
-            LinkedListNode add = new LinkedListNode(d);
-            while (n.next != null) {
-                n = n.next;
+        // Given two strings, write a method to decide if one is a permutation of the other. 
+        static bool checkPermutation(string str1, string str2) {
+            if (str1.Length != str2.Length) return false;
+            int[] check = new int[128];
+            for (int i = 0; i < str1.Length; i++) {
+                check[(int)str1[i]]++;
             }
-            n.next = add;
+            for (int i = 0; i < str2.Length; i++ ){
+                if (check[(int)str2[i]] == 0) return false;
+                check[(int)str2[i]]--;
+            }
+            return true;
         }
 
-        public void printList() {
-            LinkedListNode n = this;
-            while (n != null) {
-                Console.WriteLine(n.data);
-                n = n.next;
+        /* RLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string 
+        has sufficient space at the end to hold the additional characters, and that you are given the "true" 
+        length of the string. */
+        static string urlify(string str,int length) {
+            StringBuilder sb = new StringBuilder(str);
+            int start = length - 1;
+            int end = sb.Length - 1;
+            while (start >= 0) {
+                if (sb[start] != ' ') {
+                    sb[end] = sb[start];
+                    end--;
+                }
+                else {
+                    sb[end--] = '0';
+                    sb[end--] = '2';
+                    sb[end--] = '%';
+                }
+                start--;
             }
+            return sb.ToString();
         }
+
+
     }
 
 
