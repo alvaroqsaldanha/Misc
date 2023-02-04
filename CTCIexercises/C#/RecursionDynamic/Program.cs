985362739777c3cef3cd999908d5d9b0970a47b2
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2023-02-04 16:13:13 +0000

    Continuing CTCI

--- CTCIexercises/C#/RecursionDynamic/Program.cs
+++ CTCIexercises/C#/RecursionDynamic/Program.cs
@@ -23,8 +23,33 @@ namespace CTCI
                 dp[i] = dp[i-3] + dp[i-2] + dp[i-1]; 
             }
             return dp[n-1];
-
         }
+
+        /* Robot in a Grid: Imagine a robot sitting on the upper left corner of grid with r rows and c columns. 
+        The robot can only move in two directions, right and down, but certain cells are "off limits" such that 
+        the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to 
+        the bottom right. */
+        static int robotGrid(int[][] obstacleGrid) {
+            int rows = obstacleGrid.Length;
+            int cols = obstacleGrid[0].Length;
+            int[,] grid = new int[rows+1,cols+1];
+            grid[0,1] = 1;
+            for (int i = 1; i < rows+1; i++) {
+                for (int j = 1; j < cols+1; j++) {
+                    if (obstacleGrid[i-1][j-1] == 0) {
+                        grid[i,j] = grid[i-1,j] + grid[i,j-1];
+                    }
+                }
+            }
+            return grid[rows,cols];
+        }
+
+        /* Magic Index: A magic index in an array A[ 1 .•. n-1] is defined to be an index such that A[ i] 
+        i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+        array A. */
+        
+
+
 
     }
 }
